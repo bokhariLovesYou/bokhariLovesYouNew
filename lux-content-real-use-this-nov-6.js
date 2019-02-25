1aae15220887c177e40ec4aa04fd740320e3235f
Paras Bokhari <parasbokhari@gmail.com>
2019-02-25 01:16:04 -0500

    new-content

--- lux-content-real-use-this-nov-6.js
+++ lux-content-real-use-this-nov-6.js
@@ -1,15 +1,30 @@
 
 	// Add Content For Brand Pages Over Here As Variables
-	var hermesTitle = 'Secondhand Hermes Handbags, Scarves, Shoes & Accessories'
-	var hermesParagraph = 'Hermes is one of the top designers you’ll find here at LUX. We carry a wide variety of secondhand Hermes handbags and other items, with options that can match any look and personality. Whether you’re looking for the perfect handbag, scarf, or other Hermes product, LUX has one of the largest selections you’ll find anywhere.'
 	var chanelTitle = 'Secondhand Chanel Handbags, Dresses, Shoes & Accessories'
 	var chanelParagraph = 'Chanel is among the best designers you’ll find in our collection when shopping at LUX. Our selection of luxury secondhand Chanel bags includes a variety of unique designs. Whether you’re searching for the perfect Chanel handbag, shoe, wallet, or other product, LUX has one of the largest selections you’ll find anywhere. You won’t need to worry about being unable to find your favorite Chanel products when you shop with us today. We also promise 100% authenticity with every Chanel bag and other product from our store, as we check each item to ensure it’s authentic prior to putting it up for sale.'
+	let $chanelExtendedContent = '<h2>Secondhand Chanel Timeless Classic Flap Bags</h2>';
+	$chanelExtendedContent = '<p>Chanel Timeless Classic Flap bags are among the best Chanel has to offer in their long line of top-quality products. Here at LUX, you can get a Chanel Timeless Classic Flap that can complete your look, with top-quality materials that ensure these products are built to last and impress. All of our pre-owned Chanel products from this French brand are of the highest level of quality, and our Chanel Timeless Classic Flap bags are no exception</p>';
+	$chanelExtendedContent += '<h2>Buy a Chanel Timeless Classic Flap Today</h2>';
+	$chanelExtendedContent += '<p>For some of the best Chanel bags on the market, purchase one of our Chanel Timeless Classic Flap bags here today, along with many other Chanel products in our growing inventory.</p>';
 
 	var lvTitle = 'Secondhand Louis Vuitton Handbags, Clothing, Shoes & Accessories'
 	var lvParagraph = 'Louis Vuitton is one of the many reputable designers you’ll find here at LUX. We carry a wide range of secondhand Louis Vuitton handbags and other items, with options that can match your individual look and personality. Whether you’re looking for the perfect handbag, sandal, blouse, or other elegant Louis Vuitton item, LUX has one of the largest selections you’ll find anywhere.'
+	let $lvExtendedContent = '<h2>Secondhand Louis Vuitton Speedy Handbags</h2>';
+	$lvExtendedContent += '<p>As one of the most reputable French designers, Louis Vuitton has produced many top-quality handbag designs that can complement any look. This is why we’re happy to carry a selection of Louis Vuitton Speedy handbags along with other designs from this brand. Choose from one of our luxurious pre-owned Louis Vuitton Speedy bag designs, all of which are authentic and built to last. You can also choose from one of our many other Louis Vuitton handbags.</p>';
+	$lvExtendedContent += '<h2>Buy One of Our Secondhand Louis Vuitton Speedy Handbags Today</h2>';
+	$lvExtendedContent += '<p>Whether you want to purchase our Louis Vuitton Stephen Sprouse Graffiti Speedy handbag or another design, we have what you’re looking for when you shop here at LUX. Purchase one or more of these signature handbags here today, along with many other top-quality secondhand designer products.</p>';
 
 	var gucciTitle = 'Secondhand Gucci Handbags, Dresses, Shoes & Accessories'
 	var gucciParagraph = 'Gucci is among the top designers available in our collection here at LUX. Our selection of luxury secondhand Gucci bags includes a variety of unique designs. Whether you’re searching for the perfect Gucci monogram handbag, shoe, belt, or other product, LUX has one of the largest selections you’ll find anywhere. You’ll be able to find many of your favorite Gucci products when you shop with us today. We also promise 100% authenticity with every Gucci monogram bag and other products in our store, as we perform a check of each item to ensure its authenticity before it goes on the market.'
+	let $gucciExtendedContent = '<h2>Secondhand Gucci Marmont Bags & Pumps</h2>';
+	$gucciExtendedContent += '<p>Searching for some of the best pumps and belt bags available? In addition to many other luxurious and widely recognized products, Italian label Gucci has designed a line of Gucci Marmont bags and pumps that are of the highest level of quality and style. Here at LUX, you can shop for Gucci Marmont pumps and bags along with many other top-quality secondhand consignment from Gucci and other leading designers.</p>';
+	$gucciExtendedContent += '<h2>Purchase One of Our Gucci Marmont Bags or Pumps Today</h2>';
+	$gucciExtendedContent += '<p>For some of the best Gucci products on the market, turn to LUX today. We have a selection of high-quality secondhand Gucci Marmont belt bags and pumps, along with a wide variety of other Gucci products that can complement your look. All of our Gucci Marmont bags and other products are 100% authentic, which means you’ll always get top-quality secondhand consignment when you shop with us.</p>';
+	$gucciExtendedContent += '<h2>Secondhand Gucci Dionysus Bags</h2>';
+	$gucciExtendedContent += '<p>For some of the best Gucci products on the market, turn to LUX for a selection of top-quality consignment from this Italian brand, including these Gucci Dionysus bags. These bags can complete your look, with several designs available that are 100% authentic and built to last. Whether you’re looking for Gucci Dionysus shoulder bags or hobo bags, we have what you’re looking for when you shop at LUX.</p>';
+	$gucciExtendedContent += '<h2>Buy One or More of Our Gucci Dionysus Bags Today</h2>';
+	$gucciExtendedContent += '<p>Here at LUX, you can find some of the best Gucci Dionysus hobo bags and shoulder bags in our growing inventory, along with plenty of other great authentic Gucci products for sale. Shop here today and check out secondhand consignment in our selection from many other reputable designers.</p>';
+
 
 	var valentinoTitle = 'Secondhand Valentino Handbags, Clothing, Shoes & Accessories'
 	var valentinoParagraph = 'Valentino is among the many well-known and reputable designers available here at LUX. We carry a wide variety of pre-owned Valentino handbags and many other items, with plenty of options to match your style. Whether you’re searching for the ideal handbag, blouse, sandal, or other Valentino item, LUX has one of the most expansive selections of luxury Valentino products on the market.'
@@ -48,7 +63,18 @@
 	$alexExtendedContent += '<h2 style="margin: 25px 0;">Shop Our Alexander McQueen Clutch Purses, Clothing, and More</h2>'
 	$alexExtendedContent += '<p>Here at LUX, you’ll find plenty of pre-owned Alexander McQueen products, as well as consignment from many other widely recognized designers. You won’t need to worry about having trouble finding the perfect products for you with our expansive selection of consignment. Regardless of your particular style and tastes, we have something for everyone in our expansive selection.</p>'
 	
-	
+	const hermesTitle = 'Secondhand Hermes Kelly Handags & Belts';
+	const hermesParagraph = 'Hermes Kelly products are made using top-quality materials, along with other Hermes products, and it shows. Here at LUX, you’ll find a wide variety of Hermes Kelly bags and belts, along with a wide variety of other Hermes products. Known as one of the most elegant handbags on the market, the Hermes Kelly handbag, named after Hollywood icon and Princess of Monaco Grace Kelly, is both stylish and timeless. Hermes Kelly belts are also great accessories to complete your look.';
+	let $hermesExtendedContent = '<h2>Get a Hermes Kelly Handbag or Belt Today</h2>';
+	$hermesExtendedContent += '<p>Whether you’re looking for the perfect handbag or belt, we have what you’re looking for here at LUX. Shop our complete selection of Hermes Kelly bags and belts below, along with many other Hermes products in our expanding inventory.</p>'
+
+	const stellaTitle = 'Secondhand Stella McCartney Falabella Bags';
+	const stellaParagraph = 'Searching for the perfect handbag from one of the top designers? Stella McCartney Falabella bags are designed to complement nearly any look, with several unique and luxurious designs available here at LUX. Whether you’re looking for something more neutral or colorful, you’ll find what you need with Stella McCartney Falabella messenger bags, totes, and clutches available here in our inventory.';
+	let $stellaExtendedContent = '<h2>Buy Our Top-Quality Pre-Owned Stella McCartney Falabella Bags Today</h2>';
+	$stellaExtendedContent += '<p>For some of the best Stella McCartney Falabella totes, clutches, and other bags, turn to LUX. We have a growing selection that includes many top-quality designs that are built to last for many years. When you shop at LUX, you’ll only get the best authentic secondhand products, and our Stella McCartney Falabella bags are no exception.</p>';
+
+
+		
 	// Script Works From Here.
 	var paragraphNode = document.createElement('p');
 	var headingNode = document.createElement('h2');
@@ -73,14 +99,25 @@
 	// Conditions
 	if (breadList[2].innerText === 'Hermes' && breadList[1].innerText === 'Designers' && breadList.length > 2 && breadList.length < 4 && (splitLastURL.length === 1 || splitLastURL.indexOf('p=1') > -1)) {
 		contentReplace(hermesTitle, hermesParagraph);
+		let theKid = document.createElement('div');
+		theKid.innerHTML = $hermesExtendedContent;
+		appendParent.appendChild(theKid);
+		document.title = 'Top-Quality Hermes Kelly Products for Sale Online | LUX';
+		document.querySelector('meta[name="keywords"]').setAttribute("content", 'Hermes Kelly products are among the many popular items you’ll find here when you shop at LUX, along with many other Hermes bags, scarves, and more. Browse our selection here and shop with us today.');
 	}
 
 	if (breadList[2].innerText === 'Chanel' && breadList[1].innerText === 'Designers' && breadList.length > 2 && breadList.length < 4 && (splitLastURL.length === 1 || splitLastURL.indexOf('p=1') > -1)) {
 		contentReplace(chanelTitle, chanelParagraph);
+		let theKid = document.createElement('div');
+		theKid.innerHTML = $chanelExtendedContent;
+		appendParent.appendChild(theKid);
 	}
 
 	if (breadList[2].innerText === 'Louis Vuitton' && breadList[1].innerText === 'Designers' && breadList.length > 2 && breadList.length < 4 && (splitLastURL.length === 1 || splitLastURL.indexOf('p=1') > -1)) {
 		contentReplace(lvTitle, lvParagraph);
+		let theKid = document.createElement('div');
+		theKid.innerHTML = $lvExtendedContent;
+		appendParent.appendChild(theKid);
 		document.title = 'High-Quality Pre-Owned Louis Vuitton Handbags & Clothing';
 		document.querySelector('meta[name="keywords"]').setAttribute("content", 'Louis Vuitton is one of the most reputable and well-recognized designers on the market, and for good reason. Here at LUX, you’ll find many high-quality secondhand Louis Vuitton handbags and much more. Browse our selection of Louis Vuitton products here and shop with us today.');
 	}
@@ -88,6 +125,9 @@
 
 	if (breadList[2].innerText === 'Gucci' && breadList[1].innerText === 'Designers' && breadList.length > 2 && breadList.length < 4 && (splitLastURL.length === 1 || splitLastURL.indexOf('p=1') > -1)) {
 		contentReplace(gucciTitle, gucciParagraph);
+		let theKid = document.createElement('div');
+		theKid.innerHTML = $gucciExtendedContent;
+		appendParent.appendChild(theKid);
 		document.title = 'High-Quality Secondhand Gucci Handbags & Accessories for Sale';
 		document.querySelector('meta[name="keywords"]').setAttribute("content", 'If you’re looking for some of the best bags, shoes, belts, clothing, or other accessories from this reputable brand, you’ll find many high-quality pre-owned Gucci handbags and much more here at LUX. Browse our selection of Louis Vuitton products here and shop with us today.');
 	}
@@ -133,6 +173,15 @@
 		appendParent.appendChild(theKid);
 		document.title = 'High-Quality Pre-Owned Alexander McQueen Consignment';
 		document.querySelector('meta[name="keywords"]').setAttribute("content", 'Alexander McQueen is among the most reputable and well-known designers in the industry, as a result of their consistent quality of products. Here at LUX, you’ll find many high-quality Alexander McQueen consignment, from elegant dresses to sequined clutches.');
+	}
+
+	if (breadList[2].innerText === 'Stella McCartney' && breadList[1].innerText === 'Designers' && breadList.length > 2 && breadList.length < 4 && (splitLastURL.length === 1 || splitLastURL.indexOf('p=1') > -1)) {
+		contentReplace(stellaTitle, stellaParagraph);
+		let theKid = document.createElement('div');
+		theKid.innerHTML = $stellaExtendedContent;
+		appendParent.appendChild(theKid);
+		document.title = 'Top-Quality Secondhand Stella McCartney Falabella Bags';
+		document.querySelector('meta[name="keywords"]').setAttribute("content", 'If you want to find some of the best secondhand bags available, turn to LUX for a selection of top-quality pre-owned Stella McCartney Falabella bags, including totes and clutches.');
 	}
 
 
