cc393e3240de5170e5265dc25d563afb0fee5ab7
Paras Bokhari <parasbokhari@gmail.com>
2019-02-25 16:04:18 -0500

    Update underwaterFishLightBadge.js

--- underwaterFishLightBadge.js
+++ underwaterFishLightBadge.js
@@ -1,14 +1,5 @@
-const addStyleString = (str) => {
-  const node = document.createElement('style');
-    node.innerHTML = str;
-    document.body.appendChild(node);
-}
-
-addStyleString('#badge-parent{width:100%;height:100%;max-width:300px;margin-left:auto;margin-right:auto}');
-addStyleString('#badge-id{width:100%;height:auto}');
-
-const img = document.createElement("img");
-img.setAttribute('id', 'badge-id')
-const outerParent = document.getElementById('badge-parent')
-img.src = "https://www.underwaterfishlight.com/wp-content/uploads/2017/10/underwaterfishlight-logo.png";
-outerParent.appendChild(img);
+<!--- Embed Code For Underwater Fishlight Badge --->
+<div class="badge-parent"></div>
+<script>
+function addStyleString(t){var e=document.createElement("style");e.innerHTML=t,document.body.appendChild(e)}addStyleString("#badge-parent{width:100%;height:100%;max-width:100%;margin-top:2rem;margin-bottom:2rem;}"),addStyleString("#badge-id{width:100%;height:auto;max-width:300px;}");var img=document.createElement("img");img.src="https://www.underwaterfishlight.com/wp-content/uploads/2017/10/underwaterfishlight-logo.png",img.setAttribute("id","badge-id");var outerParent=document.querySelectorAll(".badge-parent");outerParent.forEach(function(t){t.appendChild(img)});
+</script>
